ff0a5e3f2298bd346779a082ef6e675307c2b73a
Sergey Linnik <sergey.linnik@onlyoffice.com>
2017-12-25 11:11:54 +0300

    detecting mobile

--- controller/editorcontroller.php
+++ controller/editorcontroller.php
@@ -105,6 +105,11 @@ class EditorController extends Controller {
     private $crypt;
 
     /**
+     * Mobile regex from https://github.com/ONLYOFFICE/CommunityServer/blob/v9.1.1/web/studio/ASC.Web.Studio/web.appsettings.config#L35
+     */
+    const USER_AGENT_MOBILE = "/android|avantgo|playbook|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|symbian|treo|up\\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i";
+
+    /**
      * @param string $AppName - application name
      * @param IRequest $request - request object
      * @param IRootFolder $root - root folder
@@ -376,6 +381,11 @@ class EditorController extends Controller {
             $callback = str_replace($this->urlGenerator->getAbsoluteURL("/"), $this->config->GetStorageUrl(), $callback);
         }
 
+        $type = "desktop";
+        if (\OC::$server->getRequest()->isUserAgent([$this::USER_AGENT_MOBILE])) {
+            $type = "mobile";
+        }
+
         $params = [
             "document" => [
                 "fileType" => $ext,
@@ -397,7 +407,8 @@ class EditorController extends Controller {
                     "id" => $userId,
                     "name" => $this->userSession->getUser()->getDisplayName()
                 ]
-            ]
+            ],
+            "type" => $type
         ];
 
         if (!empty($this->config->GetDocumentServerSecret())) {
