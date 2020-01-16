54dc1d118478344fc32457e440302cd8fc04738e
Sergey Linnik <sergey.linnik@onlyoffice.com>
2020-01-16 17:55:00 +0300

    copyright 2020

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,6 @@
 /**
  *
- * (c) Copyright Ascensio System SIA 2019
+ * (c) Copyright Ascensio System SIA 2020
  *
  * This program is a free software product.
  * You can redistribute it and/or modify it under the terms of the GNU Affero General Public License
