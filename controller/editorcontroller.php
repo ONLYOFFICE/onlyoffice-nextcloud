41c15da4dd9ac15a82ffe7c5fa1885140bfebe90
Sergey Linnik <sergey.linnik@onlyoffice.com>
2020-09-10 10:50:52 +0300

    forcesave by Save button

--- controller/editorcontroller.php
+++ controller/editorcontroller.php
@@ -868,6 +868,9 @@ class EditorController extends Controller {
             ],
             "documentType" => $format["type"],
             "editorConfig" => [
+                "customization" => [
+                    "forcesave" => true
+                ],
                 "lang" => str_replace("_", "-", \OC::$server->getL10NFactory("")->get("")->getLanguageCode()),
                 "region" => str_replace("_", "-", \OC::$server->getL10NFactory("")->findLocale())
             ]
