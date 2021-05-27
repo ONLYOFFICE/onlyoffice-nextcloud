26697a203aed6b0b9cdf809137dfd878daee3507
Sergey Linnik <sergey.linnik@onlyoffice.com>
2021-05-27 13:38:08 +0300

    change message after conversion

--- js/main.js
+++ js/main.js
@@ -177,7 +177,7 @@
                     fileList.add(response, { animate: true });
                 }
 
-                OCP.Toast.success(t(OCA.Onlyoffice.AppName, "File created"));
+                OCP.Toast.success(t(OCA.Onlyoffice.AppName, "File has been converted. Its content might look differently."));
             });
     };
 
