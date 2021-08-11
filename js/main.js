aec3300ff6e0ff363447cc1fb3e867578aa29a14
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2021-08-11 10:24:01 +0300

    open share link directly

--- js/main.js
+++ js/main.js
@@ -430,16 +430,27 @@
                     return;
                 }
 
-                var button = document.createElement("a");
-                button.href = OC.generateUrl("apps/" + OCA.Onlyoffice.AppName + "/s/" + encodeURIComponent($("#sharingToken").val()));
-                button.className = "onlyoffice-public-open button";
-                button.innerText = t(OCA.Onlyoffice.AppName, "Open in ONLYOFFICE")
+                var editorUrl = OC.generateUrl("apps/" + OCA.Onlyoffice.AppName + "/s/" + encodeURIComponent($("#sharingToken").val()));
 
-                if (!OCA.Onlyoffice.setting.sameTab) {
-                    button.target = "_blank";
+                if (_oc_appswebroots.richdocuments
+                    || _oc_appswebroots.files_pdfviewer && extension === "pdf"
+                    || _oc_appswebroots.text && extension === "txt") {
+
+                    var button = document.createElement("a");
+                    button.href = editorUrl;
+                    button.className = "onlyoffice-public-open button";
+                    button.innerText = t(OCA.Onlyoffice.AppName, "Open in ONLYOFFICE")
+
+                    if (!OCA.Onlyoffice.setting.sameTab) {
+                        button.target = "_blank";
+                    }
+
+                    $("#preview").prepend(button);
+                } else {
+                    OCA.Onlyoffice.frameSelector = "#onlyofficeFrame";
+                    var $iframe = $("<iframe id=\"onlyofficeFrame\" nonce=\"" + btoa(OC.requestToken) + "\" scrolling=\"no\" allowfullscreen src=\"" + editorUrl + "?inframe=true\" />");
+                    $("#app-content").append($iframe);
                 }
-
-                $("#preview").prepend(button);
             };
 
             OCA.Onlyoffice.GetSettings(initSharedButton);
