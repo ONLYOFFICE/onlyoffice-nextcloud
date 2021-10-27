dd4401a31b9754573eaaa03bcb1b31333a957471
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2021-10-27 11:58:59 +0300

    versions history don't for external link

--- controller/editorcontroller.php
+++ controller/editorcontroller.php
@@ -667,7 +667,6 @@ class EditorController extends Controller {
      * @return array
      *
      * @NoAdminRequired
-     * @PublicPage
      */
     public function history($fileId, $shareToken = null) {
         $this->logger->debug("Request history for: $fileId", ["app" => $this->appName]);
@@ -788,7 +787,6 @@ class EditorController extends Controller {
      * @return array
      *
      * @NoAdminRequired
-     * @PublicPage
      */
     public function version($fileId, $version, $shareToken = null) {
         $this->logger->debug("Request version for: $fileId ($version)", ["app" => $this->appName]);
