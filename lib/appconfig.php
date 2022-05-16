45b24e4028696bb8687e9da8e587fc9baa5058a5
Olga Larinova <olga.larinova@onlyoffice.com>
2022-05-16 11:23:12 +0300

    add date to default watermark + add anonimous in watermark

--- lib/appconfig.php
+++ lib/appconfig.php
@@ -875,7 +875,7 @@ class AppConfig {
      */
     public function GetWatermarkSettings() {
         $result = [
-            "text" => $this->config->getAppValue(AppConfig::WATERMARK_APP_NAMESPACE, "watermark_text", "{userId}"),
+            "text" => $this->config->getAppValue(AppConfig::WATERMARK_APP_NAMESPACE, "watermark_text", "{userId}, {date}"),
         ];
 
         $watermarkLabels = [
