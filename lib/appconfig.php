2af634b172e6179241487a6e76fbd67a8018ff1a
rivexe <stepan.mayorov@onlyoffice.com>
2023-03-15 14:54:25 +0300

    editors check interval added to config

--- lib/appconfig.php
+++ lib/appconfig.php
@@ -320,6 +320,13 @@ class AppConfig {
     public $_customizationPlugins = "customization_plugins";
 
     /**
+     * The config key for the interval of editors availability check by cron
+     *
+     * @var string
+     */
+    private $_editors_check_interval = "editors_check_interval";
+
+    /**
      * @param string $AppName - application name
      */
     public function __construct($AppName) {
@@ -1273,6 +1280,29 @@ class AppConfig {
      */
     public function GetCustomizationPlugins() {
         return $this->config->getAppValue($this->appName, $this->_customizationPlugins, "true") === "true";
+    }
+
+    /**
+     * Get the editors check interval
+     *
+     * @return int
+     */
+    public function GetEditorsCheckInterval() {
+        $interval = (integer)$this->GetSystemValue($this->_editors_check_interval);
+
+        if (empty($interval)) {
+            $interval = 86400; // 1 day - default value
+        }
+        return $interval;
+    }
+
+    /**
+     * Save the editors check interval setting
+     *
+     * @param int $value - editors check interval (in seconds)
+     */
+    public function SetEditorsCheckInterval($value) {
+        $this->config->setAppValue($this->appName, $this->_editorsCheck_interval, $value);
     }
 
     /**
