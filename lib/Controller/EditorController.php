55ff6b05675012a2bcb7031e16e0c62e6ce90dcf
rivexe <stepan.mayorov@onlyoffice.com>
2023-11-29 15:15:18 +0300

    controllers now pass FileInfo instances to methods of the FileVersions class

--- lib/Controller/EditorController.php
+++ lib/Controller/EditorController.php
@@ -854,7 +854,7 @@ class EditorController extends Controller {
 
             $versionId = $version->getRevisionId();
 
-            $author = FileVersions::getAuthor($ownerId, $fileId, $versionId);
+            $author = FileVersions::getAuthor($ownerId, $file->getFileInfo(), $versionId);
             $authorId = $author !== null ? $author["id"] : $ownerId;
             $authorName = $author !== null ? $author["name"] : $owner->getDisplayName();
 
@@ -863,7 +863,7 @@ class EditorController extends Controller {
                 "name" => $authorName
             ];
 
-            $historyData = FileVersions::getHistoryData($ownerId, $fileId, $versionId, $prevVersion);
+            $historyData = FileVersions::getHistoryData($ownerId, $file->getFileInfo(), $versionId, $prevVersion);
             if ($historyData !== null) {
                 $historyItem["changes"] = $historyData["changes"];
                 $historyItem["serverVersion"] = $historyData["serverVersion"];
@@ -885,7 +885,7 @@ class EditorController extends Controller {
 
         $versionId = $file->getFileInfo()->getMtime();
 
-        $author = FileVersions::getAuthor($ownerId, $fileId, $versionId);
+        $author = FileVersions::getAuthor($ownerId, $file->getFileInfo(), $versionId);
         if ($author !== null) {
             $historyItem["user"] = [
                 "id" => $this->buildUserId($author["id"]),
@@ -898,7 +898,7 @@ class EditorController extends Controller {
             ];
         }
 
-        $historyData = FileVersions::getHistoryData($ownerId, $fileId, $versionId, $prevVersion);
+        $historyData = FileVersions::getHistoryData($ownerId, $file->getFileInfo(), $versionId, $prevVersion);
         if ($historyData !== null) {
             $historyItem["changes"] = $historyData["changes"];
             $historyItem["serverVersion"] = $historyData["serverVersion"];
@@ -984,7 +984,7 @@ class EditorController extends Controller {
 
         if ($version > 1
             && count($versions) >= $version - 1
-            && FileVersions::hasChanges($ownerId, $fileId, $versionId)) {
+            && FileVersions::hasChanges($ownerId, $file->getFileInfo(), $versionId)) {
             $changesUrl = $this->getUrl($file, $user, null, $version, true);
             $result["changesUrl"] = $changesUrl;
 
