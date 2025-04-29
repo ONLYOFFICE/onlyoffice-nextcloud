7b884bd0f4aeaa99d3a7bb12e9c7856d12fe1781
rivexe <stepan.mayorov@onlyoffice.com>
2025-04-29 09:41:31 +0300

    fix: lint

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -86,7 +86,7 @@ class Application extends App implements IBootstrap {
     }
 
     public function register(IRegistrationContext $context): void {
-        require_once __DIR__ . '/../../vendor/autoload.php';
+        require_once __DIR__ . "/../../vendor/autoload.php";
 
         // Set the leeway for the JWT library in case the system clock is a second off
         \Firebase\JWT\JWT::$leeway = $this->appConfig->getJwtLeeway();
