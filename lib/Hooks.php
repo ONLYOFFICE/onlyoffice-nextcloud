7eed78c0e4a4bd948c692be8bf210a570c230dc0
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-08-15 10:33:53 +0300

    additional check

--- lib/Hooks.php
+++ lib/Hooks.php
@@ -30,6 +30,7 @@
 namespace OCA\Onlyoffice;
 
 use OC\Files\Filesystem;
+use OC\Files\Node\File;
 use OCP\Util;
 
 /**
@@ -175,6 +176,12 @@ class Hooks {
      * @param array $params - hook param
      */
     public static function fileVersionRestore($params) {
+        $node = $params["node"];
+
+        if (empty($node) || !($node instanceof File)) {
+            return;
+        }
+
         $filePath = preg_replace('/^\/\w+\/files\//', '', $params["node"]->getPath());
         if (empty($filePath)) {
             return;
