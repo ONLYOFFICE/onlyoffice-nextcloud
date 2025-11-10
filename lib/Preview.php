55ee06086ab583565a36b472bca83bbd3f77f12f
Sergey Linnik <sergey.linnik@onlyoffice.com>
2025-11-10 15:00:06 +0300

    change mask

--- lib/Preview.php
+++ lib/Preview.php
@@ -395,6 +395,6 @@ class Preview extends Provider {
 
         $fileExtension = $fileInfo->getExtension();
 
-        return [$fileUrl, $fileExtension, "thumb_" . $key];
+        return [$fileUrl, $fileExtension, "thumb_$key"];
     }
 }
