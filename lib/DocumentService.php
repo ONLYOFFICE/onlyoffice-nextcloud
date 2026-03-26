69c283fb1465981e79e9f6c8179a4904bb54e55b
krzotr <krzotr@gmail.com>
2026-03-26 15:58:40 +0100

    Replaced shardKey with shardkey (lowercase)

--- lib/DocumentService.php
+++ lib/DocumentService.php
@@ -148,7 +148,7 @@ class DocumentService {
         }
 
         $document_revision_id = self::generateRevisionId($document_revision_id);
-        $urlToConverter = $urlToConverter . "?shardKey=" . $document_revision_id;
+        $urlToConverter = $urlToConverter . "?shardkey=" . $document_revision_id;
 
         if (empty($from_extension)) {
             $from_extension = pathinfo($document_uri)["extension"];
