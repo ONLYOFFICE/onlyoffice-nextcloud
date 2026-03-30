e10f9272db310aae0a5e87319696ce89dd05bc76
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-03-30 20:08:16 +0000

    feat(e2e): add tests for directory share with edit permission

--- tests/e2e/common/share-link.spec.ts
+++ tests/e2e/common/share-link.spec.ts
@@ -1,27 +1,59 @@
-import { test } from '../fixtures'
-import { deleteFile } from '../helpers/webdav'
-import { createPublicShare, deleteShare, ShareInfo } from '../helpers/shares'
+import { test, expect } from '../fixtures'
+import { deleteFile, createDirectory } from '../helpers/webdav'
+import { createPublicShare, createPublicEditShare, deleteShare, ShareInfo } from '../helpers/shares'
 import { uploadNewTemplate } from '../helpers/templates'
+import { EditorPage } from '../fixtures/EditorPage'
+import { FilesPage } from '../fixtures/FilesPage'
 
-const FILE_NAME = 'share.docx'
-let share: ShareInfo
+test.describe('File share', () => {
+	const FILE_NAME = 'share.docx'
+	let share: ShareInfo
 
-test.beforeEach(async ({ user }) => {
-	await uploadNewTemplate('share', 'docx', user)
-	share = await createPublicShare(`/${FILE_NAME}`, user)
+	test.beforeEach(async ({ user }) => {
+		await uploadNewTemplate('share', 'docx', user)
+		share = await createPublicShare(`/${FILE_NAME}`, user)
+	})
+
+	test.afterEach(async ({ user }) => {
+		await deleteShare(share.id, user)
+		await deleteFile(`/${FILE_NAME}`, user)
+	})
+
+	test('Editor auto-opens for public docx share for user', async ({ userPage, editorPage }) => {
+		await userPage.goto(`index.php/s/${share.token}`)
+		await expect(editorPage.editorIFrame()).toBeVisible()
+		await expect(editorPage.documentContent()).toBeVisible()
+	})
+
+	test('Editor auto-opens for public docx share for guest', async ({ guestPage }) => {
+		await guestPage.goto(`index.php/s/${share.token}`)
+		const editorPage = new EditorPage(guestPage)
+		await expect(editorPage.editorIFrame()).toBeVisible()
+		await expect(editorPage.documentContent()).toBeVisible()
+	})
 })
 
-test.afterEach(async ({ user }) => {
-	await deleteShare(share.id, user)
-	await deleteFile(`/${FILE_NAME}`, user)
-})
+test.describe('Directory share with edit permission', () => {
+	const DIR_NAME = 'share-dir'
+	let share: ShareInfo
 
-test('Editor auto-opens for public docx share for user', async ({ userPage }) => {
-	await userPage.goto(`index.php/s/${share.token}`)
-	await userPage.waitForEvent('console', msg => msg.text() === 'ONLYOFFICE Editor is loaded')
-})
+	test.beforeEach(async ({ user }) => {
+		await createDirectory(`/${DIR_NAME}`, user)
+		share = await createPublicEditShare(`/${DIR_NAME}`, user)
+	})
 
-test('Editor auto-opens for public docx share for guest', async ({ guestPage }) => {
-	await guestPage.goto(`index.php/s/${share.token}`)
-	await guestPage.waitForEvent('console', msg => msg.text() === 'ONLYOFFICE Editor is loaded')
+	test.afterEach(async ({ user }) => {
+		await deleteShare(share.id, user)
+		await deleteFile(`/${DIR_NAME}`, user)
+	})
+
+	test('New file buttons are present on edit directory share', async ({ guestPage }) => {
+		await guestPage.goto(`index.php/s/${share.token}`)
+		const filesPage = new FilesPage(guestPage)
+		await filesPage.openNewMenu()
+
+		for (const fileType of ['document', 'spreadsheet', 'presentation']) {
+			await expect(filesPage.menuItem(`New ${fileType}`)).toBeVisible()
+		}
+	})
 })
