3466e65ba91864ab247ed243bbed44232aec4f6d
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-04-04 14:51:15 +0500

    fix(federation): fix null dereference in healthCheck debug log

--- lib/RemoteInstance.php
+++ lib/RemoteInstance.php
@@ -160,7 +160,7 @@ class RemoteInstance {
             self::update($remote, $status);
         }
 
-        $logger->debug("Remote instance " . $remote . " was stored to database status " . $dbremote["status"], ["app" => self::APP_NAME]);
+        $logger->debug("Remote instance " . $remote . " was stored to database status " . $status, ["app" => self::APP_NAME]);
 
         self::$healthRemote[$remote] = $status;
 
