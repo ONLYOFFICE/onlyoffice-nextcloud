b5b87e36a68bbcdd69207d93839680560a656679
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-08-17 17:21:34 +0500

    chore(editor): remove legacy comparefile event

--- src/editor.js
+++ src/editor.js
@@ -172,7 +172,6 @@ OCA.Onlyoffice.InitEditor = function() {
 					config.events.onRequestSaveAs = OCA.Onlyoffice.onRequestSaveAs
 					config.events.onRequestInsertImage = OCA.Onlyoffice.onRequestInsertImage
 					config.events.onRequestMailMergeRecipients = OCA.Onlyoffice.onRequestMailMergeRecipients
-					config.events.onRequestCompareFile = OCA.Onlyoffice.onRequestSelectDocument // todo: remove (for editors 7.4)
 					config.events.onRequestSelectDocument = OCA.Onlyoffice.onRequestSelectDocument
 					config.events.onRequestSendNotify = OCA.Onlyoffice.onRequestSendNotify
 					config.events.onRequestReferenceData = OCA.Onlyoffice.onRequestReferenceData
